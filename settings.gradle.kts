4aab653cd7cc293a80bb440f36a9595939b3aee4
catalinsx <tcatalin1337@gmail.com>
2024-02-18 14:59:03 +0200

    Creating the project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rescue4x4"
+include(":app")
+ 
